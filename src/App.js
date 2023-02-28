0181edd42a3824cc0927ae0ea6623390470748d2
AgustinDi <agustindigiacinto18@gmail.com>
2023-02-28 11:31:19 -0300

    try responsive 1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQ2ceIQu_eT75Btgc0syTczfhK9jTIyse5PN1N-07nSaK3uMH94x9vynVi_p3zmr0v5YxJb2vTwMu5z/embed?start=false&loop=false&delayms=3000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" title='calendario'></iframe>
+        <h1>Calendario Local!</h1>
+        <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQ2ceIQu_eT75Btgc0syTczfhK9jTIyse5PN1N-07nSaK3uMH94x9vynVi_p3zmr0v5YxJb2vTwMu5z/embed?start=false&loop=false&delayms=3000" width="960" height="569" allowFullScreen={true} webkitallowfullscreen="true" title='calendario'></iframe>
       </header>
     </div>
   );
