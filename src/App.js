69cae144c3de794a4b889f80266cd694be738433
AgustinDi <agustindigiacinto18@gmail.com>
2023-02-28 15:17:42 -0300

    try responsive 4

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Calendario Local!</h1>
-        <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQ2ceIQu_eT75Btgc0syTczfhK9jTIyse5PN1N-07nSaK3uMH94x9vynVi_p3zmr0v5YxJb2vTwMu5z/embed?start=false&loop=false&delayms=3000" width="960" height="569" allowFullScreen={true} webkitallowfullscreen="true" title='calendario'></iframe>
+        <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQ2ceIQu_eT75Btgc0syTczfhK9jTIyse5PN1N-07nSaK3uMH94x9vynVi_p3zmr0v5YxJb2vTwMu5z/embed?start=false&loop=true&delayms=30000" width="960" height="569" allowFullScreen={true} webkitallowfullscreen="true" title='calendario'></iframe>
+        <p>Sobre nosotros:</p>
+        <p>
+          Bienvenidos a nuestro sitio web de calendario de talleres y clases de apoyo gratuitas. 
+          Nos complace ofrecer una amplia gama de actividades educativas para personas de todas 
+          las edades e intereses. En nuestro calendario dinámico, encontrarás una variedad de talleres 
+          y clases de apoyo emocionantes programados para el próximo mes. Nuestro espacio es un lugar
+           acogedor y creativo diseñado para fomentar el aprendizaje y la colaboración.
+          </p>
       </header>
     </div>
   );
