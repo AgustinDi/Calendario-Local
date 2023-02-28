716a86ebb17481c7c1dbfb703ab36e27d2615f47
AgustinDi <agustindigiacinto18@gmail.com>
2023-02-28 10:38:20 -0300

    calendario incrustado

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQ2ceIQu_eT75Btgc0syTczfhK9jTIyse5PN1N-07nSaK3uMH94x9vynVi_p3zmr0v5YxJb2vTwMu5z/embed?start=false&loop=false&delayms=3000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" title='calendario'></iframe>
       </header>
     </div>
   );
